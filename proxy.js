79825a92b4437a7969ebf44d9641876659099bf8
Wouter Lemaire <wouter.lemaire@flexso.com>
2019-08-28 00:41:15 +0200

    prepare basic build for es6

--- proxy.js
+++ proxy.js
@@ -1,27 +1,28 @@
+
 const express = require('express'),
-    httpProxy = require('http-proxy'),
-    fs = require('fs'),
-    proxy = new httpProxy.createProxyServer();
+httpProxy = require('http-proxy'),
+fs = require('fs'),
+proxy = new httpProxy.createProxyServer();
 
 const appRoute = {
-    target: 'http://localhost:5000'
+target: 'http://localhost:5000'
 };
 const routing = JSON.parse(fs.readFileSync('./odata.json'));
 
 var allowCrossDomain = function(req, res) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization, X-Mindflash-SessionID');
-    // intercept OPTIONS method
-    if ('OPTIONS' === req.method) {
-        res.header(200);
-    } else {
-        var dirname = req.url.replace(/^\/([^\/]*).*$/, '$1');
-        var route = routing[dirname]  || appRoute;
-        console.log(req.method + ': ' + route.target + req.url);
-        proxy.web(req, res, route);
-    }
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Credentials', 'true');
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization, X-Mindflash-SessionID');
+// intercept OPTIONS method
+if ('OPTIONS' === req.method) {
+    res.header(200);
+} else {
+    var dirname = req.url.replace(/^\/([^\/]*).*$/, '$1');
+    var route = routing[dirname]  || appRoute;
+    console.log(req.method + ': ' + route.target + req.url);
+    proxy.web(req, res, route);
+}
 };
 
 var app = express();
